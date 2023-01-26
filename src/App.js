dc8c2ff2a99c7b4f753e71ab403e24cb259f3ee1
niloyabdullah <abdullahniloy149@gmail.com>
2023-01-26 22:55:11 +0600

    gitHub logIn

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { useState } from 'react';
 import './App.css';
 import app from './Firebase/firebase.init';
 const auth = getAuth(app);
 function App() {
+  const [user, setUser] = useState({});
   const provider = new GoogleAuthProvider();
   const handleGoogleSign = () => {
     // console.log('google coming soon')
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user;
+        setUser(user);
         console.log(user)
 
       })
@@ -16,11 +19,41 @@ function App() {
         console.log('error', error)
       })
   }
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .error(() => {
+        setUser({})
+      })
+  }
   return (
 
     <div className="App">
-      <button onClick={handleGoogleSign}>Google Sing</button>
-    </div>
+      {/*conditional rendering: user sing in kora thakle Sing Out Button dekhabo. r SIng out kora thakle Singn In dekhabo.  condition ? true:false  */}
+      {user.email ?
+        <button onClick={handleSignOut}>Sign Out</button> :
+        <>
+          <button onClick={handleGoogleSign}>Google Sing</button>
+
+          <button>gitHub SignIn</button>
+        </>
+
+      }
+
+      {
+        user.email &&
+        <div>
+          User Name:{user.displayName}
+          <br />
+          Email: {user.email}
+          <br />
+          Photo:
+          <img src={user.photoURL} alt="" />
+        </div>
+      }
+    </div >
   );
 }
 
--- src/__steps.js
+++ src/__steps.js
@@ -1,4 +1,6 @@
 /*
+"INITIAL SETUP"
+
 1. Visit:console.firebase.google.com
 2.create a new firebase project
 3.Visit (Go to docs)
@@ -8,13 +10,26 @@
 
 6.Install Firebase for our project : npm install firebase
 7. DANGEROUS: get firebase config and put it in firebase.init.js 
-
 8.export app from firebase.init.js
+
+-----------------------
+SETUP THE PROVIDER
+-----------------------
 9. create auth using getAuth from firebase by using app from firebase.init.js
 10. create a google auth provider.do not forget to use 'new GoogleAuthProvider()';
 11. go to firebase project >Build> Authentication > Sing In method.
 12. Enable Google sing in method.
-
+12. create a button for google sing in method with a click handler.
 13. inside the event handler, call signInWithPOpUp  with auth, provider
 14.after singInWithPopUp .then result , error
+
+----------------------------
+DISPLAY DATA
+---------------------------
+-----------------------
+Add a new sign in method
+1.enable new sing in method
+2.Create github, twitter, fb, etc app create korba.
+3. get client and secret bosaba
+---------------------
 */
